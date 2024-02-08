e49dde30f2910f604556b30a278badb9be5b44ed
59Techdev <119964476+Egit1@users.noreply.github.com>
2024-02-08 16:09:20 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+
+const showIcon = document.querySelector('.showIcons');
+const socialLinks = document.querySelector('.social_Links');
+const barIcon = document.querySelector('.fa-bars');
+const wrapper = document.querySelector('nav');
+
+showIcon.onclick = ()=> {
+    socialLinks.classList.toggle('openIcon')
+}
+
+function menuBar() {
+    barIcon.classList.toggle('fa-times');
+    wrapper.classList.toggle('menu');
+}
+
+
+
+window.addEventListener('load', function () {
+    var revealItem = document.querySelectorAll('.reveal');
+
+    for(var i = 0; i < revealItem.length; i++) {
+        
+        windowHeight = window.innerWidth;
+        revealPoint = 150;
+        Top = revealItem[i].getBoundingClientRect().top;
+
+        if(Top = windowHeight - revealPoint) {
+            revealItem[i].classList.add('active');
+        }
+
+        else {
+            revealItem[i].classList.remove('active')
+        }
+    }
+})
